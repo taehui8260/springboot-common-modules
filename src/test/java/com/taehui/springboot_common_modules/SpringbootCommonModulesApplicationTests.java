872bd4870b4fb5036d7079ff340b3b8ad4be2fcf
taehui8260 <teahui8260>
2024-12-03 16:52:33 +0900

    INSERT: 프로젝트 최초 생성

---
+++ src/test/java/com/taehui/springboot_common_modules/SpringbootCommonModulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taehui.springboot_common_modules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommonModulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
