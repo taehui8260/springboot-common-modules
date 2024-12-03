dcc9b0b430911a6051dcca440921598bcba1102c
taehui8260 <taehui8260@gmail.com>
2024-12-03 16:52:33 +0900

    INSERT: 프로젝트 최초 생성

---
+++ src/main/java/com/taehui/springboot_common_modules/SpringbootCommonModulesApplication.java
@@ -0,0 +1,13 @@
+package com.taehui.springboot_common_modules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCommonModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCommonModulesApplication.class, args);
+	}
+
+}
