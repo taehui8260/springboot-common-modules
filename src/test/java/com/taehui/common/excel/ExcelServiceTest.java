fd86e91b8011f49b1e8995a867fdf748f6cebb3c
taehui8260 <taehui8260@gmail.com>
2024-12-16 11:13:41 +0900

    UPDATE: Excel 모듈 수정
    JUnit Test 완료

--- src/test/java/com/taehui/common/excel/ExcelServiceTest.java
+++ src/test/java/com/taehui/common/excel/ExcelServiceTest.java
@@ -2,6 +2,7 @@ package com.taehui.common.excel;
 
 
 import com.taehui.common.excel.model.ExcelData;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,13 +10,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ExcelServiceTest {
 
@@ -30,8 +33,6 @@ public class ExcelServiceTest {
     }
     @Test
     void addExcelData(){
-        ExcelUtil excelUtil = new ExcelUtil();
-
         ExcelData excel = new ExcelData();
         List<Map<String, Object>> data = Arrays.asList(
                 Map.of("ID", 1, "Name", "Name_1", "Age", 22, "Country", "Germany", "Salary", 52199),
@@ -46,7 +47,6 @@ public class ExcelServiceTest {
                 Map.of("ID", 10, "Name", "Name_10", "Age", 27, "Country", "USA", "Salary", 71164)
         );
 
-
         List<String> headerList = new ArrayList<>(List.of(new String[]{"ID", "Name", "Age", "Country", "Salary"}));
         List<String> columnList = new ArrayList<>(List.of(new String[]{"ID", "Name", "Age", "Country", "Salary"}));
 
@@ -55,31 +55,61 @@ public class ExcelServiceTest {
 
     @Test
     void testExcelDownload() throws IOException {
-        // Arrange
-        MockHttpServletRequest request = new MockHttpServletRequest();
-        MockHttpServletResponse response = new MockHttpServletResponse();
-        Workbook workbook = new XSSFWorkbook();
-        workbook.createSheet("TestSheet");
+        ExcelUtil excelUtil = new ExcelUtil();
+        ExcelData excel = new ExcelData();
+
+        List<Map<String, Object>> data = Arrays.asList(
+                Map.of("ID", 1, "Name", "Name_1", "Age", 22, "Country", "Germany", "Salary", 52199),
+                Map.of("ID", 2, "Name", "Name_2", "Age", 38, "Country", "Canada", "Salary", 46658),
+                Map.of("ID", 3, "Name", "Name_3", "Age", 34, "Country", "Australia", "Salary", 41041),
+                Map.of("ID", 4, "Name", "Name_4", "Age", 37, "Country", "UK", "Salary", 96505),
+                Map.of("ID", 5, "Name", "Name_5", "Age", 41, "Country", "Germany", "Salary", 84518),
+                Map.of("ID", 6, "Name", "Name_6", "Age", 31, "Country", "USA", "Salary", 57101),
+                Map.of("ID", 7, "Name", "Name_7", "Age", 26, "Country", "USA", "Salary", 43227),
+                Map.of("ID", 8, "Name", "Name_8", "Age", 41, "Country", "USA", "Salary", 83053),
+                Map.of("ID", 9, "Name", "Name_9", "Age", 27, "Country", "Germany", "Salary", 54043),
+                Map.of("ID", 10, "Name", "Name_10", "Age", 27, "Country", "USA", "Salary", 71164)
+        );
+
+        List<String> headerList = List.of("ID", "Name", "Age", "Country", "Salary");
+        List<String> columnList = List.of("ID", "Name", "Age", "Country", "Salary");
+
+        excelService.addExcelData(excel, "sheet", "title", data, headerList, columnList);
+
 
         // Act
-        excelService.excelDownload(response, workbook, "TestExcel.xlsx");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        try (Workbook workbook = excelUtil.createExcel(excel.getTitle(), excel.getExcelData(), excel.getHeaderList(), excel.getColumnList())) {
+            //컬럼 색 변경
+            for(String titleKey: excel.getTitle().keySet()){
+                int columnIndex = 0;
+                for(String column: excel.getColumnList().get(titleKey)){
+                    if(column.contains("Salary")){
+                        excelUtil.setColumnColor(workbook.getSheet(titleKey), columnIndex, IndexedColors.YELLOW);
+                    }
+                    columnIndex++;
+                }
+            }
 
-        // Assert
-        assertEquals("attachment; filename=TestExcel.xlsx", response.getHeader("Content-Disposition"));
-        assertEquals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", response.getContentType());
+            excelService.excelDownload(response, workbook, "TestExcel");
 
-        byte[] excelData = response.getContentAsByteArray();
-        assertTrue(excelData.length > 0);
+            // Assert
+            assertEquals("attachment; filename=TestExcel.xlsx", response.getHeader("Content-Disposition"));
+            assertEquals("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", response.getContentType());
 
-        // 파일 저장 (선택)
-        try (FileOutputStream fos = new FileOutputStream("TestExcel.xlsx")) {
-            fos.write(excelData);
-        }
+            byte[] excelData = response.getContentAsByteArray();
+            assertTrue(excelData.length > 0);
 
-        // 엑셀 파일 확인
-        try (Workbook downloadedWorkbook = new XSSFWorkbook(new ByteArrayInputStream(excelData))) {
-            assertNotNull(downloadedWorkbook.getSheet("TestSheet"));
+            // 데이터를 파일로 저장 (선택)
+            try (FileOutputStream fos = new FileOutputStream("TestExcel.xlsx")) {
+                fos.write(excelData);
+            }
+
+            // 엑셀 파일 검증
+            try (Workbook downloadedWorkbook = new XSSFWorkbook(new ByteArrayInputStream(excelData))) {
+                assertNotNull(downloadedWorkbook.getSheet("sheet"));
+                assertEquals("sheet", downloadedWorkbook.getSheetAt(0).getSheetName());
+            }
         }
     }
-
 }
